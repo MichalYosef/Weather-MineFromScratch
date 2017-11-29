e5cacdff6653943af7ac62da5f5ab3cc1958173e
Michal Yosef <ilovebreslev@gmail.com>
2017-11-29 14:03:33 +0200

    `

--- app/appCodeSchoolTut.js
+++ app/appCodeSchoolTut.js
@@ -4,23 +4,23 @@
   
   app.controller('StoreController',['$http', function($http)
   {
-    var store = this;
-    store.products = [];
-    
-    $http.get('/store-products.json').success(function(data)
-    {
-      store.products = data;
-    });
+      var store = this;
+      store.products = [];
+      
+      $http.get('store-products.json').then(function(data)
+      {
+          store.products = data;
+      });
   }]);
 
   app.controller('ReviewController', function() {
-    this.review = {};
-
-    this.addReview = function(product) {
-      product.reviews.push(this.review);
-
       this.review = {};
-    };
+
+      this.addReview = function(product) {
+        product.reviews.push(this.review);
+
+        this.review = {};
+      };
   });
 
   
